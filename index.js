64fd96d5b4b91384570b2bc3047eae7980d6b2c3
chaima marzouk <marzouk.chaima.892@gmail.com>
2021-12-15 17:20:08 +0100

    create database and connected successfully

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+
 const http = require('http');
+const mysql = require('mysql');
+
 const hostname = 'localhost';
-const port = 3000;
+const port = 5000;
+
+
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database:'to_do_list'
+});
+
+db.connect( (error) => {
+    if(error){
+        console.log("something went wrong with your database :(")
+    }
+    else{
+        console.log("You're database has been connected sucefully :)") 
+    }
+});
 const server = http.createServer((req, res) => {
- console.log(req.headers);
- res.statusCode = 200;
- res.end('<html><body><h1>Hello, World!</h1></body></html>');
-})
-server.listen(port, hostname);
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.end('<html><body><h1>Hello, World!</h1></body></html>');
+   })
+   server.listen(port, hostname);
